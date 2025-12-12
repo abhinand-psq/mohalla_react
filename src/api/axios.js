626015592677d6f91341b2ac2a7bb1fb1aad7b76
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2025-12-12 21:38:57 +0530

    error handling v1

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_LOCALHOST_URL,
+    baseURL: import.meta.env.VITE_ENV == "production" ? import.meta.env.VITE_BACKEND_API_URL : import.meta.env.VITE_LOCALHOST_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
